bdad0e8174303ee6c8fb1e25fc074c32d381d3e3
Zach Hanson <zachhanson94@gmail.com>
2021-01-26 13:59:13 -0800

    Initial Commit

---
+++ FiveSixTwoInterview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FiveSixTwoInterview
+//
+//  Created by Zach Hanson on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
